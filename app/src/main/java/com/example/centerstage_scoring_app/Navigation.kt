a4695798b7f639efa67ba1f89b9d51d6e3dd0503
justanotherinternetguy <96833060+justanotherinternetguy@users.noreply.github.com>
2024-01-20 23:21:31 -0500

    I AM GOING INSANE I SPENT 10 MINUTES DEBUGGING A SINGLE MISSING CURLY BRACE HELP

--- app/src/main/java/com/example/centerstage_scoring_app/Navigation.kt
+++ app/src/main/java/com/example/centerstage_scoring_app/Navigation.kt
@@ -3,8 +3,27 @@
 package com.example.centerstage_scoring_app
 
 import androidx.camera.view.video.ExperimentalVideo
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -20,3 +39,42 @@ fun Navigation() {
     }
 }
 
+@Composable
+fun MainScreen(navController: NavController) {
+    var text by remember {
+        mutableStateOf("")
+    }
+    Column (
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 50.dp)
+    ) {
+        TextField(
+            value = text,
+            onValueChange = {
+                text = it
+            },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Button(
+            onClick = {
+                navController.navigate(Screen.DetailScreen.route)
+            },
+            modifier = Modifier.align(Alignment.End)
+        ) {
+            Text(text = "To DetailScreen")
+        }
+    }
+}
+
+@Composable
+fun DetailScreen(name: String?) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(text = "Hello, $name")
+    }
+}
